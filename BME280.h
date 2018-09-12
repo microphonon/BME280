39223321c21aba30084e8bafcb01c7af5a996ce7
MicroPhonon <mph@microphonon.com>
2018-09-12 11:38:36 -0600

    Add files via upload

--- BME280.h
+++ BME280.h
@@ -1,51 +1,27 @@
 /*
- 	BME280.h
-	Library of 6 functions to extract and process data from the BME280 temperature-humidity sensor using 
-	the SPI protocol with the MSP430X5XX/MSP430X6XX MCU family.
-	February 2018	
+ * BME280.h
+ Library of 6 functions to extract and process data from the BME280 temperature-humidity sensor using 
+ the SPI protocol with the MSP430X5XX/MSP430X6XX MCU family.  Modify registers ctrl_hum, ctrl_meas, and config
+ as needed.  Consult the BME280 device documentation.
  */
 
 #ifndef BME280_H_
 #define BME280_H_
-#define BYTES 20 //Max number of bytes on the SPI
-#define CS_TH BIT5; //Set SPI chip select to P1.5 on MCU.  Must also be set consistently in main program
+#define BYTES 20 //Max number of bytes sent on SPI
+#define CS_TH BIT5; //Set CSB to P1.5 on MCU.  Must also be set consistently in main program
 
-volatile uint32_t RawTemp;
-volatile uint16_t dig_T1,RawHumid;
+volatile int32_t RawTemp, RawPress, RawHumid;
+volatile uint16_t dig_T1, dig_P1;
 volatile int16_t  dig_T2, dig_T3;
-volatile int32_t t_fine, CorT, CorH;
+volatile int16_t  dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9;
+volatile int32_t t_fine;
 volatile uint8_t dig_H1, dig_H3, i;
 volatile int8_t dig_H6;
 volatile int16_t dig_H2, dig_H4, dig_H5;
 volatile unsigned char  Tempbuf[30], Humbuf[15];
 char RXbuf[BYTES], RXdata[BYTES], TXbuf[BYTES], Data[BYTES];
 
-void SetTHsensor(void)
-/* 	Write to registers as follows:
- 	ctrl_humid register write 0x72; Data 0x01 for 1x oversampling
- 	ctrl_meas register write 0x74; Data 0x21 for forced mode, 1x oversampling
- 	ctrl_meas MUST be written after setting ctrl_humid to enable changes
- 	config register forced to zero just in case. No filter is used as recommended
- 	Write only. No useful data is read on SPI MISO line 
-*/
-  {
-	 unsigned char TS[]={0x72,0x01,0x74,0x21,0x75,0x00};
-	 UCB0CTL1 &= ~UCSWRST; //Start USCI
- 	 P1OUT &= ~CS_TH; //Pull CSB line low
- 	
- 	 for (i=0; i < sizeof TS; i++)
- 	 {
- 		 while (!(UCB0IFG & UCTXIFG)); //Check if it is OK to write
- 		 UCB0TXBUF = TS[i]; //Load data into transmit buffer
- 		 while (!(UCB0IFG & UCRXIFG)); //Wait until complete RX byte is received
- 		 RXbuf[i] = UCB0RXBUF; //Read buffer to clear RX flag. Data is not used
- 	 }
- 	 P1OUT |= CS_TH; //Pull CSB line high
- 	 UCB0CTL1 |= UCSWRST; //Stop USCI
-  }
-
-uint8_t ReadTHid(void)  
-//Get the TH sensor chip ID = 0x60. If not found, set FLAG
+uint8_t ReadTHid(void)  // Get the TH sensor chip ID: 0x60
     {
 	 uint8_t FLAG;
 	 UCB0CTL1 &= ~UCSWRST; //Start USCI
@@ -67,26 +43,27 @@ uint8_t ReadTHid(void)
     }
 
 void GetCompData(void)
-/* 	Compensation data can be read in sleep mode. Use burst read to get temp, pressure, and 
-	humidity compensation bytes starting at first data byte 0X88.
-    	Retrieve data while CSB is low. 
-*/
      {
-		UCB0CTL1 &= ~UCSWRST; //Start USCI
-		P1OUT &= ~CS_TH; //Pull CSB line low
+    	/* Compensation data can be read in sleep mode. Use burst read
+    	to get temp, pressure, and humidity compensation bytes starting at first data byte 0X88.
+    	Retrieve data while CSB is low. */
 
-		while (!(UCB0IFG & UCTXIFG)); 
-		UCB0TXBUF = 0x88; 
-		while (!(UCB0IFG & UCRXIFG)); 
-		Tempbuf[0] = UCB0RXBUF; 
-		//Burst mode read of temp, press, and first humidity compensation bytes follows
-		for (i=1; i < 27; i++)
-		{
-			while (!(UCB0IFG & UCTXIFG));
-			UCB0TXBUF = 0xAA; //Load dummy data into transmit buffer
-			while (!(UCB0IFG & UCRXIFG));
-			Tempbuf[i] = UCB0RXBUF;
-    		}
+	UCB0CTL1 &= ~UCSWRST; //Start USCI
+	P1OUT &= ~CS_TH; //Pull CSB line low
+
+	while (!(UCB0IFG & UCTXIFG)); //Check if it is OK to write
+	UCB0TXBUF = 0x88; //Load first address to be read.
+	while (!(UCB0IFG & UCRXIFG)); //Wait until complete RX byte is received
+	Tempbuf[0] = UCB0RXBUF; //Read buffer to clear RX flag. The first read byte is of no use
+
+	//Burst mode read of temperature, pressure, and first humidity compensation bytes follows
+	for (i=1; i < 27; i++)
+	{
+		while (!(UCB0IFG & UCTXIFG));
+		UCB0TXBUF = 0xAA; //Load dummy data into transmit buffer
+		while (!(UCB0IFG & UCRXIFG));
+		Tempbuf[i] = UCB0RXBUF;
+    	}
     	 P1OUT |= CS_TH; //Pull CSB line high
     	 UCB0CTL1 |= UCSWRST; //Stop USCI
 
@@ -95,12 +72,22 @@ void GetCompData(void)
     	dig_T2 = (int16_t)((uint16_t)(Tempbuf[4] << 8) | (uint16_t)Tempbuf[3]); //signed 16-bit int
     	dig_T3 = (int16_t)((uint16_t)(Tempbuf[6] << 8) | (uint16_t)Tempbuf[5]); //signed 16-bit int
 
+    	// Build the pressure compensation coefficients
+    	dig_P1 = ((uint16_t)(Tempbuf[8] << 8) | (uint16_t)Tempbuf[7]); //unsigned 16-bit int
+    	dig_P2 = (int16_t)((uint16_t)(Tempbuf[10] << 8) | (uint16_t)Tempbuf[9]); //signed 16-bit int
+    	dig_P3 = (int16_t)((uint16_t)(Tempbuf[12] << 8) | (uint16_t)Tempbuf[11]); //signed 16-bit int
+    	dig_P4 = (int16_t)((uint16_t)(Tempbuf[14] << 8) | (uint16_t)Tempbuf[13]); //signed 16-bit int
+    	dig_P5 = (int16_t)((uint16_t)(Tempbuf[16] << 8) | (uint16_t)Tempbuf[15]); //signed 16-bit int
+    	dig_P6 = (int16_t)((uint16_t)(Tempbuf[18] << 8) | (uint16_t)Tempbuf[17]); //signed 16-bit int
+    	dig_P7 = (int16_t)((uint16_t)(Tempbuf[20] << 8) | (uint16_t)Tempbuf[19]); //signed 16-bit int
+    	dig_P8 = (int16_t)((uint16_t)(Tempbuf[22] << 8) | (uint16_t)Tempbuf[21]); //signed 16-bit int
+    	dig_P9 = (int16_t)((uint16_t)(Tempbuf[24] << 8) | (uint16_t)Tempbuf[23]); //signed 16-bit int
+
     	Humbuf[1]=Tempbuf[26]; //This is byte 0xA1
 
-    	//Burst read of remaining humidity bytes
     	UCB0CTL1 &= ~UCSWRST; //Start USCI
     	P1OUT &= ~CS_TH; //Pull CSB line low
-
+    	//Burst read of remaining humidity bytes follows
     	while (!(UCB0IFG & UCTXIFG));
     	UCB0TXBUF = 0xE1; //Load first read address
     	while (!(UCB0IFG & UCRXIFG));
@@ -108,7 +95,7 @@ void GetCompData(void)
 
     	for (i=2; i < 10; i++) //Write dummy variable to produce burst read
     	{
-       		while (!(UCB0IFG & UCTXIFG));
+      		while (!(UCB0IFG & UCTXIFG));
              	UCB0TXBUF = 0xAA; //Load dummy data into transmit buffer
              	while (!(UCB0IFG & UCRXIFG));
              	Humbuf[i] = UCB0RXBUF;
@@ -121,8 +108,7 @@ void GetCompData(void)
     	 dig_H1 = (uint8_t)Humbuf[1]; //0xA1
     	 dig_H2 = (int16_t)((uint16_t)(Humbuf[3] << 8) | (uint16_t)Humbuf[2]); //  0xE2/0xE1
     	 dig_H3 = (uint8_t)Humbuf[4];  //0xE3
-    	 /* dig_H4 and dig_H5 use the lower and upper nibbles of 0xE5, respectively. Split up 
-		Humbuf[6] for this purpose */
+    	 //dig_H4 and dig_H5 use the lower and upper nibbles of 0xE5, respectively. Split up Humbuf[6] for this purpose
     	 dig_H4 = (int16_t)((uint16_t)(Humbuf[5] << 4) | (uint16_t)(Humbuf[6] & 0x0F)); //0xE4 / low nibble of 0xE5 (12 bits)
     	 dig_H5 = (int16_t)((uint16_t)(Humbuf[7] << 4) | (uint16_t)((Humbuf[6] >>4) & 0x0F)); //0xE7 / high nibble of 0xE5 (12 bits)
     	 dig_H6 = (uint8_t)Humbuf[8]; //0xE7
@@ -130,64 +116,105 @@ void GetCompData(void)
 
 void ReadTHsensor(void)
    {
-  	/* Wake-up sensor by writing 0x74; Data 0x21 for forced mode, 1x scaling. Use recommended burst
-  	mode read by sending address of first data byte 0XF7. Retrieve data while CSB is low. */
-  	 unsigned char TH1[]={0x74,0x22,0xF7};
+  	/* Read from sensor as follows: The ctrl_hum register is written with 0x72 followed by 0x01 for 1x oversampling.
+ 	The ctrl_meas register is written with 0x74; send 0x21 for temperature only, forced mode, 1x oversampling.
+	Send 0x25 for pressure + temperature, forced mode, 1x oversampling.
+	Sending 0x74 also wakes up the semsor and enables any changes written to the ctrl_humid register. The config 
+	register 0x75 is forced to zero just in case. No filter is used as recommended for low rate polling.
+  	Use recommended burst mode read of 8 data registers by sending address of first byte 0XF7. Retrieve
+  	data while CSB is low. */
+
+	 //unsigned char TH1[]={0x72,0x01,0x74,0x21,0x75,0x00,0xF7}; //Temperature only
+  	 unsigned char TH1[]={0x72,0x01,0x74,0x25,0x75,0x00,0xF7}; //Temp + pressure
   	 UCB0CTL1 &= ~UCSWRST; //Start USCI
   	 P1OUT &= ~CS_TH; //Pull CSB line low
 
   	 for (i=0; i < sizeof TH1; i++) //Write above data via SPI.  No useful data is read but save anyway
   	 {
-  		 while (!(UCB0IFG & UCTXIFG)); 
-  		 UCB0TXBUF = TH1[i]; 
-  		 while (!(UCB0IFG & UCRXIFG)); 
+  		 while (!(UCB0IFG & UCTXIFG)); //Check if it is OK to write
+  		 UCB0TXBUF = TH1[i]; //Load data into transmit buffer
+  		 while (!(UCB0IFG & UCRXIFG)); //Wait until complete RX byte is received
   		 RXbuf[i] = UCB0RXBUF; //Read buffer to clear RX flag. These bytes are of no use
   	 }
-  	/* Burst read of 8 data bytes (press, temp, humid) from BME280. 0xF7 Data[0] is already queued in register.
-  	 Data[3]: Temperature  MSB
-  	 Data[4]: Temperature LSB
-  	 Data[5]: Temperature XLSB
-  	 Data[6]: Humidity MSB
-  	 Data[7]: Humidity LSB
-  	 */
+  	// Now do a burst read of 8 data bytes (press, temp, humid) from BME280.
   	for (i=0; i < 9; i++) //Burst read.  Address 0x07 auto-increments
-  		{
-  			while (!(UCB0IFG & UCTXIFG)); 
-  			UCB0TXBUF = 0xAA; //Load dummy data into transmit buffer
-  			while (!(UCB0IFG & UCRXIFG)); //Wait until complete RX byte is received
-  			Data[i] = UCB0RXBUF; //Read buffer to clear RX flag
-  		}
+  	{
+  		while (!(UCB0IFG & UCTXIFG)); //Check if it is OK to write
+  		UCB0TXBUF = 0xAA; //Load dummy data into transmit buffer
+  		while (!(UCB0IFG & UCRXIFG)); //Wait until complete RX byte is received
+  		Data[i] = UCB0RXBUF; //Read buffer to clear RX flag
+  	}
  	 P1OUT |= CS_TH; //Pull CSB line high
  	 UCB0CTL1 |= UCSWRST; //Stop USCI
+	/*
+	Data[0]: Pressure  MSB
+  	Data[1]: Pressure LSB
+  	Data[2]: Pressure XLSB
+  	Data[3]: Temperature  MSB
+  	Data[4]: Temperature LSB
+  	Data[5]: Temperature XLSB
+  	Data[6]: Humidity MSB
+  	Data[7]: Humidity LSB
 
-  	// Assemble the data bytes to make a 20-bit long integer for temperature; 16-bit integer for humidity 
+	Assemble the data bytes to make a 20-bit long integer for pressure and temperature and a 16-bit integer for humidity */
+ 	RawPress = ((uint32_t)Data[0] << 16 | (uint32_t)Data[1] << 8 | Data[2]) >> 4; //20-bit long unsigned integer
   	RawTemp = ((uint32_t)Data[3] << 16 | (uint32_t)Data[4] << 8 | Data[5]) >> 4; //20-bit long unsigned integer
  	RawHumid = ((uint32_t)Data[6] << 8) | (uint32_t)Data[7]; //16-bit unsigned integer
    }
 
-int32_t CalcTemp(void) //Implement integer conversion formula from BME280 spec sheet
-   {
-  	 	 volatile int32_t var1, var2, T;
-  	 	 var1 = (((((int32_t)RawTemp >> 3) - ((int32_t)dig_T1 << 1))) * (int32_t)dig_T2) >> 11;
-  	 	 var2 = (((int32_t)RawTemp >> 4) - (int32_t)dig_T1);
-  	 	 var2 = (((var2*var2) >> 12) * (int32_t)dig_T3) >> 14;
-  	 	 t_fine = var1 + var2;
-  	 	 T = ((t_fine * 5) + 128) >> 8;
-  	 	 return T;
-   }
+//The following functions make integer data conversions appropriate for the MSP430
+
+int32_t CalcTemp(void) //32-bit integer conversion formula from BME280 spec sheet
+{
+  	volatile int32_t var1, var2, T;
+  	var1 = (((((int32_t)RawTemp >> 3) - ((int32_t)dig_T1 << 1))) * (int32_t)dig_T2) >> 11;
+  	var2 = (((int32_t)RawTemp >> 4) - (int32_t)dig_T1);
+  	var2 = (((var2*var2) >> 12) * (int32_t)dig_T3) >> 14;
+  	t_fine = var1 + var2;
+  	T = ((t_fine * 5) + 128) >> 8;
+  	return T;
+}
 
 uint32_t CalcHumid(void) //Implement integer conversion formula from BME280 spec sheet
-   {
-  	 	 volatile int32_t var3;
-  	 	 var3 = t_fine - (int32_t)76800;
-  	 	 var3 = ((((((int32_t)RawHumid << 14) - (((int32_t)dig_H4) << 20) - (((int32_t)dig_H5) * var3)) +
-  			((int32_t)16384)) >> 15) * (((((((var3 * ((int32_t)dig_H6)) >> 10) * (((var3 *
-  			((int32_t)dig_H3)) >> 11) + ((int32_t)32768))) >> 10) + ((int32_t)2097152)) *
-  			((int32_t)dig_H2) + (int32_t)8192) >> 14));
-  	 	 var3 = (var3 - (((((var3 >> 15) * (var3 >> 15)) >> 7) * ((int32_t)dig_H1)) >> 4));
-  	 	 if(var3 < 0) var3 = 0;
-  	 	 if(var3 > 419430400) var3 = 419430400;
-  	 	 return (uint32_t)(var3 >> 12);
-   }
+{
+  	volatile int32_t var3;
+  	var3 = t_fine - (int32_t)76800;
+  	var3 = ((((((int32_t)RawHumid << 14) - (((int32_t)dig_H4) << 20) - (((int32_t)dig_H5) * var3)) +
+  		((int32_t)16384)) >> 15) * (((((((var3 * ((int32_t)dig_H6)) >> 10) * (((var3 *
+  		((int32_t)dig_H3)) >> 11) + ((int32_t)32768))) >> 10) + ((int32_t)2097152)) *
+  		((int32_t)dig_H2) + (int32_t)8192) >> 14));
+  	var3 = (var3 - (((((var3 >> 15) * (var3 >> 15)) >> 7) * ((int32_t)dig_H1)) >> 4));
+  	if(var3 < 0) var3 = 0;
+  	if(var3 > 419430400) var3 = 419430400;
+  	return (uint32_t)(var3 >> 12);
+}
 
+uint32_t CalcPress(void) //32-bit integer conversion formula from BME280 spec sheet
+{
+	volatile int32_t var4, var5;
+	volatile uint32_t p;
+	var4 = (((int32_t)t_fine)>>1) - (int32_t)0xFA00;
+	var5 = (((var4>>2) * (var4>>2)) >> 11 ) * ((int32_t)dig_P6);
+	var5 = var5 + ((var4*((int32_t)dig_P5))<<1);
+	var5 = (var5>>2)+(((int32_t)dig_P4)<<16);
+	var4 = (((dig_P3 * (((var4>>2) * (var4>>2)) >> 13 )) >> 3) + ((((int32_t)dig_P2) * var4)>>1))>>18;
+	var4 = ((((0x8000+var4))*((int32_t)dig_P1))>>15);
+	if (var4 == 0)
+	{
+		return 0; // Avoid exception caused by division by zero
+	}
+	p = (((uint32_t)(((int32_t)0x100000)-RawPress)-(var5>>12)))*0xC35;
+	if (p < 0x80000000)
+	{
+		p = (p << 1) / ((uint32_t)var4);
+	}
+	else
+	{
+		p = (p / (uint32_t)var4) * 2;
+	}
+	var4 = (((int32_t)dig_P9) * ((int32_t)(((p>>3) * (p>>3))>>13)))>>12;
+	var5 = (((int32_t)(p>>2)) * ((int32_t)dig_P8))>>13;
+	p = (uint32_t)((int32_t)p + ((var4 + var5 + dig_P7) >> 4));
+	return p;
+}
 #endif /* BME280_H_ */
--- BME280_SPI.c
+++ BME280_SPI.c
@@ -4,12 +4,11 @@
 #include "BME280.h"
 
 /*
- Read and display temperature and relative humidity with BME280 sensor on F5529 Launchpad.  
+ Read and display temperature, relative humidity, and pressure with BME280 sensor on F5529 Launchpad.  
  Sensor polled in forced mode using the SPI interface. Internal trimming parameters must be
  read from device to perform conversion of raw data.  Data displayed on terminal program. 
  Set serial port for 9600 baud, 8-bits, 1 stop, no parity, no flow control. UART interface is 
- on TXD (P3.3) and RXD (P3.4); these ports are reversed on the receiving device, ie. the RX-TX 
- are switched on the PC interface cable. UART polling with no RX interrupt. Green LED on P4.7 
+ on TXD (P3.3) and RXD (P3.4); UART polling with no RX interrupt. Green LED on P4.7 
  illuminates during data transmission. SPI lines are on P3.0 (MOSI), P3.1 (MISO), P3.2 (CLK), 
  and P1.5 (CS) of UCB0 module. SPI clock 1 MHz.The CS line must be configured consistently in
  the BME280.h file. Main loop runs with timed interrupt from LPM3 and VLO clock. IDE with 
@@ -21,8 +20,6 @@
 	P3.3  TXD  UART
 	P3.4  RXD  UART
 	P1.5  CS  SPI
-
-February 2018
  */
 
 # define PERIOD 10000 //Samping period. 10000 count is approximately 1 second; maximum is 65535
@@ -36,6 +33,8 @@ void SetSPI(void);
 //Variables for UART terminal display
 char str[80];
 volatile uint8_t i,count;
+volatile int32_t CorT;
+volatile uint32_t CorH, CorP;
 
 void main(void) {
 
@@ -56,9 +55,7 @@ void main(void) {
     	while(1){}
     }
 
-    //Setup the BME280 for temp and humidity measurement in forced mode
-    SetTHsensor();
-    //Get the compensation coefficients from device for raw data conversion
+    //Get the compensation coefficients from sensor for raw data conversion
     GetCompData();
 
     while(1)
@@ -66,13 +63,15 @@ void main(void) {
     	 TA0CCR0 = PERIOD; // Polling period
     	 LPM3;		//Wait in low power mode
     	 P4OUT |= BIT7; //Timeout. Turn on green LED on Launchpad
-    	 ReadTHsensor(); //Burst read on SPI to get 3 temp data bytes and 2 humidity data bytes
+    	 //Burst read on SPI to get 3 press data bytes, 3 temp bytes and 2 humidity bytes
+    	 ReadTHsensor();
     	 //Apply cal factors to raw data
     	 CorT = CalcTemp(); //Corrected temperature
     	 CorH = CalcHumid(); //Corrected humidity
+    	 CorP = CalcPress(); //Corrected pressure
     	 //Send data to serial port for display
-    	 sprintf(str,"%s %lu.%.2lu%s %lu.%.2lu%s", "Temperature:", CorT/100, CorT%100,"C Rel Humidity:",
-    			 (int32_t)(CorH/1000), (int32_t)(CorH%100),"%\r\n\n");
+    	 sprintf(str,"%s %lu.%.2lu%s %lu.%.2lu%s %lu.%.2lu%s", "Temperature:", CorT/100, CorT%100,"C Rel Humidity:",
+    			 CorH/1000, CorH%100,"% Pressure:",CorP/100, CorP%100," hPa\r\n\n");
     	 count = sizeof str;
     	 for (i=0; i < count; i++)
     	 {
@@ -83,60 +82,60 @@ void main(void) {
     }
 }
 
-#pragma vector=TIMER0_A0_VECTOR //This vector name is in header file
- __interrupt void timerfoo (void) //The name of this interrupt (timerfoo) is arbitrary
+#pragma vector=TIMER0_A0_VECTOR 
+ __interrupt void timerfoo (void) 
  {
-	 LPM3_EXIT;
+	LPM3_EXIT;
  }
 
  void SetPins(void)
   {
-	 	 /* Port 1
-			P1.0 Red LED
- 			P1.5 TH Select.  Pull this CSB line low to enable BME280 communication
- 			CS_TH must also be defined in BME280.h
-	 	  */
- 	    	P1DIR |= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
- 	    	P1OUT &= ~BIT0; //LED off
+	/* Port 1
+	P1.0 Red LED
+ 	P1.5 TH Select.  Pull this CSB line low to enable BME280 communication
+ 	CS_TH must also be defined in BME280.h
+	*/
+ 	P1DIR |= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
+ 	P1OUT &= ~BIT0; //LED off
 
- 	    	/* Port 2
- 	    	P2.1  Button on Launchpad
- 	    	 */
- 	    	P2DIR |= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
+ 	/* Port 2
+ 	P2.1  Button on Launchpad
+ 	*/
+ 	P2DIR |= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
 
- 	    	/* Port 3 */
- 	    	P3SEL = BIT0 + BIT1 + BIT2 + BIT3 + BIT4; //SPI + UART lines
- 	    	P3DIR |= BIT5 + BIT6 + BIT7; //Unused lines
+ 	/* Port 3 */
+ 	P3SEL = BIT0 + BIT1 + BIT2 + BIT3 + BIT4; //SPI + UART lines
+ 	P3DIR |= BIT5 + BIT6 + BIT7; //Unused lines
 
- 	    	/* Port 4
- 	   		P4.0--4.6 unused
- 	   		P4.7 Green LED
- 	    	 */
- 	   	    P4DIR |= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
- 	   	    P4OUT &= ~BIT7;
+ 	/* Port 4
+ 	P4.0--4.6 unused
+ 	P4.7 Green LED
+ 	*/
+ 	P4DIR |= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
+ 	P4OUT &= ~BIT7;
 
- 	   	 /* Port 5
- 	   	    P5.0 Unused
- 	   	    P5.1 Unused
- 	   	    P5.2--P5.5 grounded or open as per spec sheet
- 	   	  */
- 	   	    P5DIR |= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
+ 	/* Port 5
+ 	P5.0 Unused
+ 	P5.1 Unused
+ 	P5.2--P5.5 grounded or open as per spec sheet
+ 	*/
+ 	P5DIR |= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
 
- 	   	/* Port 6
- 	   		P6.0--6.7 unused
- 	   	*/
- 	   	    P6DIR |= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
+ 	/* Port 6
+ 	P6.0--6.7 unused
+ 	*/
+ 	P6DIR |= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7;
   }
 
  void SetVLO(void)
     { //Default frequency ~ 10 kHz
-	 	 UCSCTL4 |= SELA_1;  //Set ACLK to VLO
+	UCSCTL4 |= SELA_1;  //Set ACLK to VLO
     }
 
  void SetTimer(void)
      {
- 	 	TA0CCTL0 |= CCIE;  //Enable timer interrupt
- 	 	TA0CTL = TASSEL_1 | MC_1;  //Set Timer A to ACLK; MC_1 to count up to TA0CCR0.
+ 	TA0CCTL0 |= CCIE;  //Enable timer interrupt
+ 	TA0CTL = TASSEL_1 | MC_1;  //Set Timer A to ACLK; MC_1 to count up to TA0CCR0.
      }
 
  void SetUART(void)
@@ -158,4 +157,5 @@ void main(void) {
 	 UCB0BR0 |= 0x01; //1 MHz SPI clock
 	 UCB0BR1 |= 0x00;
    }
+
 
